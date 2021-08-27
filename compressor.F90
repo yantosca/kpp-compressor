90d374fc71663667cb567303fddb1a53340ac7dc
Michael Long <msl3v@virginia.edu>
2021-08-27 12:12:47 -0400

    Fix DO_JVS -- msl

--- compressor.F90
+++ compressor.F90
@@ -114,6 +114,8 @@ program main
      IF (ANY(REMOVE.eq.LU_IROW(i)).or.ANY(REMOVE.eq.LU_ICOL(i))) THEN
         rLU_ICOL(i) = 0
         rLU_IROW(i) = 0
+        ! Toggle DO_JVS()
+        tDO_JVS(i) = .false. ! Turn off this term in Jac_SP()
      ELSE
         idx=idx+1
         cLU_IROW(idx) = iSPC_MAP(LU_IROW(i))
@@ -121,10 +123,6 @@ program main
         cLU_ICOL(idx) = iSPC_MAP(LU_ICOL(i))
         rLU_ICOL(i)   = LU_ICOL(i)
         JVS_MAP(idx)  = i
-     ENDIF
-     ! Toggle DO_JVS()
-     IF (LU_IROW(i).eq.REMOVE(S).or.LU_ICOL(i).eq.REMOVE(S)) THEN
-        tDO_JVS(i) = .false. ! Turn off this term in Jac_SP()
      ENDIF
   ENDDO
 
